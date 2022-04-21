9aef0ba315eb98f8e6b8fdbc76491b9dace18008
Benjamin Ly <97613650+ly-benjamin@users.noreply.github.com>
2022-04-21 12:16:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "aws_vpc" "BenVPC" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "Ben_main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Ben-Terraform-test"
   }
 }
